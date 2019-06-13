435870d9875ef893f7ea7d1de4f6fd4a65acf986
DESKTOP-NBA1BI2\Admin <nizhnichenko92@gmail.com>
2019-06-13 23:59:53 +0300

    initial commit for EventBus

---
+++ EventBusRabbitMQ/DefaultRabbitMQPersistentConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace EventBusRabbitMQ
+{
+	public class DefaultRabbitMQPersistentConnection: IRabbitMQPersistentConnection
+	{
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsConnected { get; }
+		public bool TryConnect()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IModel CreateModel()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EventBus.Abstractions;
+using EventBus.Events;
+
+namespace EventBusRabbitMQ
+{
+	public class EventBusRabbitMQ: IEventBus
+	{
+		public void Publish(IntegrationEvent @event)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SubscribeDynamic<TH>(string eventName) where TH : IDynamicIntegrationEventHandler
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UnsubscribeDynamic<TH>(string eventName) where TH : IDynamicIntegrationEventHandler
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Unsubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
