fb23dfe99555660d45770adc39a8c97fe0c5ed5f
DESKTOP-NBA1BI2\Admin <nizhnichenko92@gmail.com>
2019-06-18 10:41:21 +0300

    EventBus changes

--- EventBusRabbitMQ/EventBusRabbitMQ.cs
+++ EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Text;
 using EventBus.Abstractions;
 using EventBus.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace EventBusRabbitMQ
 {
@@ -8,7 +11,22 @@ namespace EventBusRabbitMQ
 	{
 		public void Publish(IntegrationEvent @event)
 		{
-			throw new NotImplementedException();
+			//{
+			//	var eventName = @event.GetType().Name;
+			//	var factory = new ConnectionFactory() { HostName = _connectionString };
+			//	using (var connection = factory.CreateConnection())
+			//	using (var channel = connection.CreateModel())
+			//	{
+			//		channel.ExchangeDeclare(exchange: _brokerName,
+			//			type: "direct");
+			//		string message = JsonConvert.SerializeObject(@event);
+			//		var body = Encoding.UTF8.GetBytes(message);
+			//		channel.BasicPublish(exchange: _brokerName,
+			//			routingKey: eventName,
+			//			basicProperties: null,
+			//			body: body);
+			//	}
+			//}
 		}
 
 		public void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>
